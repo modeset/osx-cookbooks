b4c3e9f892d1dec8d83ad8f2918ed55ba647958b
Joshua Peek <josh@joshpeek.com>
2011-02-23 09:04:16 -0600

    Only upgrade outdated

--- homebrew/recipes/upgrade.rb
+++ homebrew/recipes/upgrade.rb
@@ -1,6 +1,6 @@
 include_recipe "homebrew"
 
-`#{node[:homebrew][:prefix]}/bin/brew list`.lines.each do |line|
+`#{node[:homebrew][:prefix]}/bin/brew outdated`.lines.each do |line|
   package line.chomp do
     action :upgrade
     notifies :run, resources(:execute => "brew cleanup"), :delayed
