bc9cd6708c65444434abe109f6f20d5a9f7d5ab5
Joshua Peek <josh@joshpeek.com>
2011-02-23 14:29:57 -0600

    Rename to postgresql

---
+++ postgresql/attributes/default.rb
@@ -0,0 +1,1 @@
+default.postgresql[:launchd] = false
--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -1,13 +1,13 @@
 include_recipe "homebrew"
 
-package "postgres"
+package "postgresql"
 
 execute "initdb #{node[:homebrew][:prefix]}/var/postgres" do
   user node[:homebrew][:user]
   creates "#{node[:homebrew][:prefix]}/var/postgres"
 end
 
-if node[:postgres][:launchd]
+if node[:postgresql][:launchd]
   launch_service "org.postgresql.postgres" do
     template_variables :prefix => node[:homebrew][:prefix],
       :user => node[:homebrew][:user]
