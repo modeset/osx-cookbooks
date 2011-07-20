70f1f0c0163b673f4ca13fc3b70e955ef9f39689
Joshua Peek <josh@joshpeek.com>
2011-07-20 15:37:32 -0500

    Remove some unused fonts

--- fonts/recipes/monofur.rb
+++
@@ -1,9 +0,0 @@
-# http://www.dafont.com/monofur.font
-
-%w( monof55 monof56 ).each do |filename|
-  cookbook_file "#{node[:fonts][:path]}/#{filename}.ttf" do
-    mode 0444
-    owner node[:fonts][:user]
-    group "staff"
-  end
-end
