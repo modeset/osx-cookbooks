d2d009a339cd41610420f0caae107cad56f4c0f9
Joshua Peek <josh@joshpeek.com>
2012-07-31 02:32:10 -0500

    Remove shell default

--- defaults/recipes/terminal.rb
+++ defaults/recipes/terminal.rb
@@ -1,7 +1,3 @@
-defaults "com.apple.Terminal", "Shell" do
-  value node[:apple][:terminal][:shell]
-end
-
 defaults "com.apple.Terminal", "Default Window Settings" do
   value node[:apple][:terminal][:default_settings]
 end
