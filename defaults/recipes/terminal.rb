4d941089070976f1214ef879dba3a5e27c7b4a69
Joshua Peek <josh@joshpeek.com>
2012-07-31 02:31:36 -0500

    Add terminal window settings

--- defaults/recipes/terminal.rb
+++ defaults/recipes/terminal.rb
@@ -1,3 +1,11 @@
 defaults "com.apple.Terminal", "Shell" do
   value node[:apple][:terminal][:shell]
 end
+
+defaults "com.apple.Terminal", "Default Window Settings" do
+  value node[:apple][:terminal][:default_settings]
+end
+
+defaults "com.apple.Terminal", "Startup Window Settings" do
+  value node[:apple][:terminal][:default_settings]
+end
