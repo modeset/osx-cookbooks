87e875b8bdc84015b38f44bb6d45bbd70d94663b
Joshua Peek <josh@joshpeek.com>
2012-07-31 01:52:57 -0500

    Ensure brew exists before running outdated check

--- homebrew/recipes/upgrade.rb
+++ homebrew/recipes/upgrade.rb
@@ -1,8 +1,10 @@
 include_recipe "homebrew"
 
-`#{node[:homebrew][:prefix]}/bin/brew outdated`.lines.each do |line|
-  package line.chomp do
-    action :upgrade
-    notifies :run, resources(:execute => "brew cleanup"), :delayed
+if File.exist?("#{node[:homebrew][:prefix]}/bin/brew")
+  `#{node[:homebrew][:prefix]}/bin/brew outdated`.lines.each do |line|
+    package line.chomp do
+      action :upgrade
+      notifies :run, resources(:execute => "brew cleanup"), :delayed
+    end
   end
 end
