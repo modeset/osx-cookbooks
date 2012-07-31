0a32526310419ee0d73d01d1099c81eb5ad831c8
Joshua Peek <josh@joshpeek.com>
2012-07-31 02:59:04 -0500

    Install custom term settings from url

--- defaults/recipes/terminal.rb
+++ defaults/recipes/terminal.rb
@@ -5,3 +5,24 @@ end
 defaults "com.apple.Terminal", "Startup Window Settings" do
   value node[:apple][:terminal][:default_settings]
 end
+
+if settings = node[:apple][:terminal][:settings]
+  installed_settings = `defaults read com.apple.Terminal "Window Settings"`
+  for url in settings
+    filename = File.basename(url)
+    name     = URI.unescape(File.basename(filename, ".terminal"))
+    target   = "#{Chef::Config[:file_cache_path]}/#{filename}"
+
+    unless installed_settings.match(name)
+      remote_file target do
+        source url
+        owner node[:defaults][:user]
+        group "staff"
+        action :create_if_missing
+      end
+      execute "install #{name}" do
+        command "open #{target.inspect}"
+      end
+    end
+  end
+end
