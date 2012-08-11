1289a28da6c579cca351b7e22b6cea154ce7932b
Joshua Peek <josh@joshpeek.com>
2012-08-11 14:25:29 -0500

    Only define command once

--- git/recipes/repositories.rb
+++ git/recipes/repositories.rb
@@ -5,13 +5,15 @@ node[:git][:repositories].each do |dir, url|
     user node[:git][:user]
   end
 
-  execute "git checkout master" do
+  execute "#{dir} checkout master" do
+    command "git checkout master"
     only_if { File.read("#{dir}/.git/HEAD") != "ref: refs/heads/master\n" }
     user node[:git][:user]
     cwd dir
   end
 
-  execute "git branch --delete deploy" do
+  execute "#{dir} branch --delete deploy" do
+    command "git branch --delete deploy"
     only_if { File.exist?("#{dir}/.git/refs/heads/deploy") }
     user node[:git][:user]
     cwd dir
