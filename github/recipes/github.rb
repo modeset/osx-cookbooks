da973a54c82a81106533a94125cbca44cec07fdd
Joshua Peek <josh@joshpeek.com>
2012-08-19 12:16:10 -0500

    github app deps

---
+++ github/recipes/github.rb
@@ -0,0 +1,18 @@
+include_recipe "erlang"
+include_recipe "gcc"
+include_recipe "git"
+include_recipe "homebrew"
+include_recipe "icu4c"
+include_recipe "memcached"
+include_recipe "mysql"
+include_recipe "node"
+include_recipe "openvpn"
+include_recipe "pow"
+include_recipe "python"
+include_recipe "rbenv"
+include_recipe "redis"
+include_recipe "solr"
+
+package "gpgme"
+
+rbenv_version "1.8.7-p358"
